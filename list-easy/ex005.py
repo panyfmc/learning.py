92318a4bbe8acda476f8f18fe83ddbbfcabf371e
pany <153379216+panyfmc@users.noreply.github.com>
2025-01-22 17:29:49 -0300

    :sparkles: feat: testando loop em uma range

---
+++ list-easy/ex005.py
@@ -0,0 +1,8 @@
+# Loop: printar o quadrado de cada número antecessor ao número dado, para números não-negativos i < n = i ^ 2
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    for n in range(0, 5):
+        print(n**2)
+        
