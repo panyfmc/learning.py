cef3d47be026970440fde8cde7074985969a499d
pany <153379216+panyfmc@users.noreply.github.com>
2025-01-22 16:16:08 -0300

    :sparkles: feat: return formato int and float

---
+++ ex004.py
@@ -0,0 +1,8 @@
+# Basicamente retornar a divisão entre a & b no formato int & float (muito fácil)
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(int(a / b))
+    print(float(a / b))
