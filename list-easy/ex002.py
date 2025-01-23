a2d93e1795e14222b03c02a8ccbcd093a9402926
panyfmc <paulavanessa010@gmail.com>
2025-01-23 17:00:00 -0300

    :truck: chore: renomeando

--- list-easy/ex002.py
+++
@@ -1,5 +0,0 @@
-# Programa que leia o nome de uma pessoa e manda uma mensagem de boas vindas
-
-nome = input("Digite seu nome: ")
-
-print(f"Olá seja bem vindo(a) {nome}!")
--- list-easy/ex004.py
+++
@@ -1,8 +0,0 @@
-# Basicamente retornar a divisão entre a & b no formato int & float (muito fácil)
-
-
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-    print(int(a / b))
-    print(float(a / b))
